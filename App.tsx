373c35423dcd03f64a716c6470bcdcfc133cc800
vokrob <106639488+vokrob@users.noreply.github.com>
2025-04-07 23:11:18 +0700

    feat: Create a basic app structure

--- App.tsx
+++ App.tsx
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Danil Borkov</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  }
+);
